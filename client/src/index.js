6ff8f48389f2326c0215eef780d29c236428c64f
German <germanhernandez.dr@gmail.com>
2017-09-25 11:51:57 +0100

    added scss

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import axios from 'axios';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/main.css';
 
 import App from './components/App';
 import reducers from './reducers';
